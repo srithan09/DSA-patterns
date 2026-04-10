8657b1b760b18d996c125fb318a2ed40a7502877
Srithan M S <123616762+srithan09@users.noreply.github.com>
2026-04-10 12:56:08 +0530

    Trees-Day2

---
+++ BinaryTrees/diameter/diameter.java
@@ -0,0 +1,16 @@
+class TreeNode
+{
+    int data;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int data)
+    {
+        this.data = data;
+    }
+}
+public class diameter {
+    
+    public static void main(String[] args) {
+        
+    }
+}
